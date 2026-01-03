cf0a232bbc52f7c90e315b2e74fc81a33a798cfd
esteban-Rz <estebanalmanque@gmail.com>
2026-01-02 19:09:12 -0500

    primer modelo

---
+++ js/carrito.js
@@ -0,0 +1,58 @@
+let cart = [];
+
+// Referencias
+const botonCompraUno = document.getElementById("boton1");
+const botonCompraDos = document.getElementById("boton2");
+const botonCompraTres = document.getElementById("boton3");
+const botonMostrarCompras = document.getElementById("mostrarCompras");
+const contadorCompra = document.getElementById("contador-carrito");
+const imprimirCompras = document.getElementById("imprimirCompras");
+const botonlimpiar = document.getElementById ("limpiar");
+
+// Productos
+const productoUno = {
+  id: 1,
+  name: "Lata de comida para perro gourmet",
+  price: 4
+};
+const productoDos = {
+  id: 2,
+  name: "Caja multiproteínas para perros 10 unidades",
+  price: 40
+};
+const productoTres = {
+  id: 3,
+  name: "Lata de comida para gato",
+  price: 6
+};
+
+// Eventos de compra
+botonCompraUno.addEventListener("click", () => {
+  cart.push(productoUno);
+  contadorCompra.textContent = "Carrito: " + cart.length;
+  console.log("Carrito actual:", cart);
+});
+
+botonCompraDos.addEventListener("click", () => {
+  cart.push(productoDos);
+  contadorCompra.textContent = "Carrito: " + cart.length;
+  console.log("Carrito actual:", cart);
+});
+
+botonCompraTres.addEventListener("click", () => {
+  cart.push(productoTres);
+  contadorCompra.textContent = "Carrito: " + cart.length;
+  console.log("Carrito actual:", cart);
+});
+
+// Mostrar lista
+botonMostrarCompras.addEventListener("click", () => {
+  imprimirCompras.innerHTML = ""; // limpiar lista
+  cart.forEach((item) => {
+    const li = document.createElement("li");
+    li.textContent = `${item.name} - $${item.price}`;
+    imprimirCompras.appendChild(li);
+  });
+});
+botonlimpiar.addEventListener("click",() => {
+  imprimirCompras.innerHTML = "";})
